d33b1fb2bcb72a6289a7e6e3f0e0ec5b39c90fec
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2023-12-12 08:52:56 -0400

    Add files via upload

---
+++ matriz/matriz/nent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz
+{
+    class nent
+    {
+        private int n;
+        public nent()
+        {
+            n = 0;
+        }
+        public void cargar(int dato)
+        {
+            n = dato;
+        }
+
+        public int descargar()
+        {
+            return n;
+        }
+        public bool esPrimo()
+        {
+            if (n < 2)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
